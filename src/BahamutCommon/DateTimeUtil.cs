652b7e3c0f76cacfc23f60d827fcc10b74370722
AlexChow <cplover@live.com>
2015-11-16 22:36:09 +0800

    update

--- src/BahamutCommon/DateTimeUtil.cs
+++ src/BahamutCommon/DateTimeUtil.cs
@@ -7,6 +7,13 @@ namespace BahamutCommon
 {
     public class DateTimeUtil
     {
+
+        public static int ConvertDateTimeSecondInt(DateTime dt)
+        {
+            var time = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            return (int)(dt - time).TotalSeconds;
+        }
+
         public static string ToAccurateDateTimeString(DateTime date)
         {
             return date.ToString("yyyy-MM-dd HH:mm:ss.fff");
