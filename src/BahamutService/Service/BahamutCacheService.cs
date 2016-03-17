6c899addd8cc066aa9d5f080e144eec0f7dc2e9d
AlexChow <cplover@live.com>
2016-03-17 21:01:29 +0800

    separation cache service from pb service

---
+++ src/BahamutService/Service/BahamutCacheService.cs
@@ -0,0 +1,88 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BahamutService.Service
+{
+    public class BahamutCacheModel
+    {
+        public string AppUniqueId { get; set; }
+        public string Type { get; set; }
+        public string UniqueId { get; set; }
+        public string DeserializableString { get; set; }
+        public DateTime CacheDateTime { get; set; }
+        public string ExtraInfo { get; set; }
+
+        public string GetCacheModelListKey()
+        {
+            return GenerateCacheModelListKey(AppUniqueId, Type, UniqueId);
+        }
+
+        public static string GenerateCacheModelListKey(string appUniqueId, string modelType, string uniqueId)
+        {
+            return string.Format("{0}:{1}:{2}", appUniqueId, modelType, uniqueId);
+        }
+
+    }
+
+    public class BahamutCacheService
+    {
+        private IRedisClientsManager mcClientManager;
+        public BahamutCacheService(IRedisClientsManager mcClientManager)
+        {
+            this.mcClientManager = mcClientManager;
+        }
+
+        public void PushCacheModelToList(BahamutCacheModel model)
+        {
+            Task.Run(() =>
+            {
+                using (var msgClient = mcClientManager.GetClient())
+                {
+                    var client = msgClient.As<BahamutCacheModel>();
+
+                    var key = model.GetCacheModelListKey();
+                    var list = client.Lists[key];
+                    list.Append(model);
+
+                }
+            });
+        }
+
+
+        public void ClearCacheModels(string appUniqueId, string modelType, string uniqueId)
+        {
+            Task.Run(() =>
+            {
+                using (var msgClient = mcClientManager.GetClient())
+                {
+                    var client = msgClient.As<BahamutCacheModel>();
+                    var key = BahamutCacheModel.GenerateCacheModelListKey(appUniqueId, modelType, uniqueId);
+                    var list = client.Lists[key];
+                    if (list == null)
+                    {
+                        return;
+                    }
+                    client.RemoveAllFromList(list);
+                }
+            });
+        }
+
+        public async Task<IEnumerable<BahamutCacheModel>> GetCacheModels(string appUniqueId, string modelType, string uniqueId)
+        {
+            return await Task.Run(() =>
+            {
+                using (var msgClient = mcClientManager.GetClient())
+                {
+                    var client = msgClient.As<BahamutCacheModel>();
+                    var key = BahamutCacheModel.GenerateCacheModelListKey(appUniqueId, modelType, uniqueId);
+                    return client.Lists[key].Where(m => m.Type == modelType && m.UniqueId == uniqueId);
+                }
+            });
+        }
+    }
+
+    
+}
--- src/BahamutService/Service/BahamutPubSubService.cs
+++ src/BahamutService/Service/BahamutPubSubService.cs
@@ -7,13 +7,6 @@ using System.Threading.Tasks;
 
 namespace BahamutService.Service
 {
-    public class BahamutUserAppNotifyMessage
-    {
-        public string NotificationType { get; set; }
-        public string DeserializableMessage { get; set; }
-        public DateTime PublishDateTime { get; set; }
-        public string ExtraInfo { get; set; }
-    }
 
     public class BahamutPublishModel
     {
@@ -25,74 +18,39 @@ namespace BahamutService.Service
     public class BahamutPubSubService
     {
         public static readonly string UnSubscribeMessage = "UnSubscribe";
-        private IRedisClientsManager mcClientManager;
+        
 
         private IRedisClientsManager psClientManager { get; set; }
 
-        public BahamutPubSubService(IRedisClientsManager psClientManager, IRedisClientsManager mcClientManager)
+        public BahamutPubSubService(IRedisClientsManager psClientManager)
         {
-            this.mcClientManager = mcClientManager;
             this.psClientManager = psClientManager;
         }
 
-        public void PublishBahamutUserNotifyMessage(string appUniqueId, string toUserId, BahamutUserAppNotifyMessage message)
+        public void PublishBahamutUserNotifyMessage(string appUniqueId, BahamutPublishModel message)
         {
+            if (string.IsNullOrWhiteSpace(appUniqueId))
+            {
+                throw new Exception("App Id Is Empty");
+            }
+            if (string.IsNullOrWhiteSpace(message.ToUser))
+            {
+                throw new Exception("To User Is Empty");
+            }
+            if (string.IsNullOrWhiteSpace(message.NotifyType))
+            {
+                throw new Exception("Notify Type Is Empty");
+            }
             Task.Run(() =>
             {
                 using (var psClient = psClientManager.GetClient())
                 {
-                    using (var msgClient = mcClientManager.GetClient())
-                    {
-                        var client = msgClient.As<BahamutUserAppNotifyMessage>();
+                    psClient.PublishMessage(appUniqueId, JsonConvert.SerializeObject(message));
 
-                        var key = GenerateMessageListKey(appUniqueId, message.NotificationType, toUserId);
-                        var list = client.Lists[key];
-                        list.Append(message);
-                        var notifyMsg = new BahamutPublishModel()
-                        {
-                            ToUser = toUserId,
-                            NotifyType = message.NotificationType,
-                            Info = message.ExtraInfo
-                        };
-                        psClient.PublishMessage(appUniqueId, JsonConvert.SerializeObject(notifyMsg));
-
-                    }
                 }
             });
         }
-
-        public void PublishBahamutUserNotifyMessages(string appUniqueId, string toUserId, IEnumerable<BahamutUserAppNotifyMessage> messages)
-        {
-            Task.Run(() =>
-            {
-                using (var psClient = psClientManager.GetClient())
-                {
-                    using (var msgClient = mcClientManager.GetClient())
-                    {
-                        var client = msgClient.As<BahamutUserAppNotifyMessage>();
-                        foreach (var msg in messages)
-                        {
-                            var key = GenerateMessageListKey(appUniqueId, msg.NotificationType, toUserId);
-                            var list = client.Lists[key];
-                            list.Append(msg);
-                            var notifyMsg = new BahamutPublishModel()
-                            {
-                                ToUser = toUserId,
-                                NotifyType = msg.NotificationType,
-                                Info = msg.ExtraInfo
-                            };
-                            psClient.PublishMessage(toUserId, JsonConvert.SerializeObject(notifyMsg));
-                        }
-                    }
-                }
-            });
-        }
-
-        private static string GenerateMessageListKey(string appUniqueId, string notifyType,string userId)
-        {
-            return string.Format("{0}:{1}:{2}", appUniqueId, notifyType, userId);
-        }
-
+        
         public void UnSubscribe(string channel)
         {
             psClientManager.GetClient().PublishMessage(channel, UnSubscribeMessage);
@@ -101,43 +59,6 @@ namespace BahamutService.Service
         public IRedisSubscription CreateSubscription()
         {
             return psClientManager.GetClient().CreateSubscription();
-        }
-
-        public async Task<IEnumerable<BahamutUserAppNotifyMessage>> GetBahamutUserNotifyMessages(string appUniqueId, string userId, string notificationType)
-        {
-            return await Task.Run(() =>
-            {
-                using (var psClient = psClientManager.GetClient())
-                {
-                    using (var msgClient = mcClientManager.GetClient())
-                    {
-                        var client = msgClient.As<BahamutUserAppNotifyMessage>();
-                        var key = GenerateMessageListKey(appUniqueId, notificationType, userId);
-                        return client.Lists[key].Where(m => m.NotificationType == notificationType);
-                    }
-                }
-            });
-        }
-
-        public void ClearBahamutUserNotifyMessages(string appUniqueId, string userId, string notificationType)
-        {
-            Task.Run(() =>
-            {
-                using (var psClient = psClientManager.GetClient())
-                {
-                    using (var msgClient = mcClientManager.GetClient())
-                    {
-                        var client = msgClient.As<BahamutUserAppNotifyMessage>();
-                        var key = GenerateMessageListKey(appUniqueId, notificationType, userId);
-                        var list = client.Lists[key];
-                        if (list == null)
-                        {
-                            return;
-                        }
-                        client.RemoveAllFromList(list);
-                    }
-                }
-            });
         }
 
         public BahamutPublishModel DeserializePublishMessage(string message)
