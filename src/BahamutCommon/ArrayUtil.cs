5468fdeaef847b4115029905832a872c75f9fdc2
AlexChow <cplover@live.com>
2016-05-12 20:55:25 +0800

    add array util class

---
+++ src/BahamutCommon/ArrayUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BahamutCommon
+{
+    public class ArrayUtil
+    {
+        public static T[] GetRandomArray<T>(T[] inputArray)
+        {
+            return GetRandomList(new List<T>(inputArray)).ToArray();
+        }
+
+        public static List<T> GetRandomList<T>(List<T> inputList)
+        {
+            //Copy to a array
+            T[] copyArray = new T[inputList.Count];
+            inputList.CopyTo(copyArray);
+
+            //Add range
+            List<T> copyList = new List<T>();
+            copyList.AddRange(copyArray);
+
+            //Set outputList and random
+            List<T> outputList = new List<T>();
+            Random rd = new Random(DateTime.Now.Millisecond);
+
+            while (copyList.Count > 0)
+            {
+                //Select an index and item
+                int rdIndex = rd.Next(0, copyList.Count - 1);
+                T remove = copyList[rdIndex];
+
+                //remove it from copyList and add it to output
+                copyList.Remove(remove);
+                outputList.Add(remove);
+            }
+            return outputList;
+        }
+    }
+}
