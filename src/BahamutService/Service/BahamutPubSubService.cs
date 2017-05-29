749dc8ed89b1d42cea9c6a482f4c3cccb4c33a29
alexchow <cplover@live.com>
2017-05-29 09:58:06 +0800

    update

--- src/BahamutService/Service/BahamutPubSubService.cs
+++ src/BahamutService/Service/BahamutPubSubService.cs
@@ -14,10 +14,29 @@ namespace BahamutService.Service
     public class BahamutPublishModel
     {
         public string ToUser { get; set; }
+        public string Appkey { get; set; }
         public string NotifyType { get; set; }
         public string Info { get; set; }
         public string NotifyInfo { get; set; }
         public string CustomCmd { get; set; }
+    }
+
+    public class BahamutPubSubUtil
+    {
+        public static string GeneratePubSubKey(string appKey, string userId)
+        {
+            return string.Format("pskey:{0}_{1}", appKey, userId);
+        }
+
+        public static string RemoveChannelPrefix(string channelWithPrefix)
+        {
+            return channelWithPrefix.Replace("btpbs_", "");
+        }
+
+        public static string AddChannelWithPrefix(string channel)
+        {
+            return string.Format("btpbs_{0}", channel);
+        }
     }
 
     public class BahamutPubSubService
@@ -30,39 +49,60 @@ namespace BahamutService.Service
             this.pubsubRedis = redis;
         }
 
-        public async Task<bool> RemoveUserDeviceAsync(string userId)
+        public async Task<bool> RemoveUserDeviceAsync(string appKey, string userId)
         {
-            return await pubsubRedis.GetDatabase().KeyDeleteAsync(userId);
+            var key = BahamutPubSubUtil.GeneratePubSubKey(appKey, userId);
+            return await pubsubRedis.GetDatabase().KeyDeleteAsync(key);
         }
 
-        public async Task<bool> RegistUserDeviceAsync(string userId, DeviceToken deviceToken, TimeSpan expireTime)
+        public async Task<bool> RegistUserDeviceAsync(string appKey, string userId, DeviceToken deviceToken, TimeSpan expireTime)
         {
+            var key = BahamutPubSubUtil.GeneratePubSubKey(appKey, userId);
             var dt = JsonConvert.SerializeObject(deviceToken, Formatting.None);
-            return await pubsubRedis.GetDatabase().StringSetAsync(userId, dt, expireTime);
+            return await pubsubRedis.GetDatabase().StringSetAsync(key, dt, expireTime);
         }
 
-        public async Task<Tuple<DeviceToken,TimeSpan>> GetUserDeviceTokenWithExpiryAsync(string userId)
+        public async Task<Tuple<DeviceToken, TimeSpan>> GetUserDeviceTokenWithExpiryAsync(string appKey, string userId)
         {
-            var dt = await pubsubRedis.GetDatabase().StringGetWithExpiryAsync(userId, CommandFlags.PreferSlave);
+            var key = BahamutPubSubUtil.GeneratePubSubKey(appKey, userId);
+
+            var dt = await pubsubRedis.GetDatabase().StringGetWithExpiryAsync(key, CommandFlags.PreferSlave);
+
             if (!string.IsNullOrWhiteSpace(dt.Value) && dt.Expiry.HasValue)
             {
                 return new Tuple<DeviceToken, TimeSpan>(JsonConvert.DeserializeObject<DeviceToken>(dt.Value), dt.Expiry.Value);
             }
-            return null;   
+            else
+            {
+                //Old version compatible
+                dt = await pubsubRedis.GetDatabase().StringGetWithExpiryAsync(userId, CommandFlags.PreferSlave);
+                if (!string.IsNullOrWhiteSpace(dt.Value) && dt.Expiry.HasValue)
+                {
+                    var dtobj = JsonConvert.DeserializeObject<DeviceToken>(dt.Value);
+                    await RegistUserDeviceAsync(appKey, userId, dtobj, dt.Expiry.Value);
+                    return new Tuple<DeviceToken, TimeSpan>(dtobj, dt.Expiry.Value);
+                }
+            }
+            return null;
         }
 
-        public async Task<DeviceToken> GetUserDeviceTokenAsync(string userId)
+        public async Task<DeviceToken> GetUserDeviceTokenAsync(string appKey, string userId)
         {
-            var dt = await pubsubRedis.GetDatabase().StringGetAsync(userId, CommandFlags.PreferSlave);
-            return JsonToDeviceToken(dt);
+            var key = BahamutPubSubUtil.GeneratePubSubKey(appKey, userId);
+            var dt = await pubsubRedis.GetDatabase().StringGetAsync(key, CommandFlags.PreferSlave);
+            if (!string.IsNullOrWhiteSpace(dt))
+            {
+                return JsonToDeviceToken(dt);
+            }
+            return null;
         }
 
-        public async Task<IEnumerable<DeviceToken>> GetUserDeviceTokensAsync(IEnumerable<string> userIds)
+        public async Task<IEnumerable<DeviceToken>> GetUserDeviceTokensAsync(string appKey, IEnumerable<string> userIds)
         {
             var keys = new RedisKey[userIds.Count()];
             for (int i = 0; i < userIds.Count(); i++)
             {
-                keys[i] = userIds.ElementAt(i);
+                keys[i] = BahamutPubSubUtil.GeneratePubSubKey(appKey, userIds.ElementAt(i));
             }
             var dtJsons = await pubsubRedis.GetDatabase().StringGetAsync(keys.ToArray(), CommandFlags.PreferSlave);
             return from item in dtJsons select JsonToDeviceToken(item);
@@ -79,20 +119,22 @@ namespace BahamutService.Service
             {
                 return new DeviceToken();
             }
-            
+
         }
 
-        public async Task<TimeSpan> GetDeviceTokenTimeToLiveAsync(string userId)
+        public async Task<TimeSpan> GetDeviceTokenTimeToLiveAsync(string appKey, string userId)
         {
-            var ts = await pubsubRedis.GetDatabase().KeyTimeToLiveAsync(userId);
+            var key = BahamutPubSubUtil.GeneratePubSubKey(appKey, userId);
+            var ts = await pubsubRedis.GetDatabase().KeyTimeToLiveAsync(key);
             return ts.HasValue ? ts.Value : TimeSpan.FromSeconds(0);
         }
 
-        public async Task<DeviceToken> GetUserDeviceTokenAsync(string userId, TimeSpan expireTime)
+        public async Task<DeviceToken> GetUserDeviceTokenAsync(string appKey, string userId, TimeSpan expireTime)
         {
             var db = pubsubRedis.GetDatabase();
-            await db.KeyExpireAsync(userId, expireTime);
-            var dt = await db.StringGetAsync(userId, CommandFlags.PreferSlave);
+            var key = BahamutPubSubUtil.GeneratePubSubKey(appKey, userId);
+            await db.KeyExpireAsync(key, expireTime);
+            var dt = await db.StringGetAsync(key, CommandFlags.PreferSlave);
             if (!string.IsNullOrWhiteSpace(dt))
             {
                 return JsonConvert.DeserializeObject<DeviceToken>(dt);
@@ -100,27 +142,18 @@ namespace BahamutService.Service
             return null;
         }
 
-        private static string RemoveChannelPrefix(string channelWithPrefix)
-        {
-            return channelWithPrefix.Replace("btpbs_", "");
-        }
-
-        private static string AddChannelWithPrefix(string channel)
-        {
-            return string.Format("btpbs_{0}", channel);
-        }
-
         public async Task SubscribeAsync(RedisChannel channel, Action<RedisChannel, RedisValue> action)
         {
-            await pubsubRedis.GetSubscriber().SubscribeAsync(AddChannelWithPrefix(channel), (c, v) =>
+            await pubsubRedis.GetSubscriber().SubscribeAsync(BahamutPubSubUtil.AddChannelWithPrefix(channel), (c, v) =>
             {
-                action(RemoveChannelPrefix(c), v);
+                action(BahamutPubSubUtil.RemoveChannelPrefix(c), v);
             });
         }
 
-        public async Task<bool> ExpireUserDeviceTokenAsync(string userId, TimeSpan expireTime)
+        public async Task<bool> ExpireUserDeviceTokenAsync(string appKey, string userId, TimeSpan expireTime)
         {
-            return await pubsubRedis.GetDatabase().KeyExpireAsync(userId, expireTime);
+            var key = BahamutPubSubUtil.GeneratePubSubKey(appKey, userId);
+            return await pubsubRedis.GetDatabase().KeyExpireAsync(key, expireTime);
         }
 
         public void PublishBahamutUserNotifyMessage(string appChannel, BahamutPublishModel message)
@@ -137,17 +170,21 @@ namespace BahamutService.Service
             {
                 throw new Exception("Notify Type Is Empty");
             }
+            if (string.IsNullOrWhiteSpace(message.Appkey))
+            {
+                throw new Exception("BahamutPublishModel Appkey Is Empty");
+            }
             Task.Run(async () =>
             {
-                await pubsubRedis.GetDatabase().PublishAsync(AddChannelWithPrefix(appChannel), JsonConvert.SerializeObject(message, Formatting.None));
+                await pubsubRedis.GetDatabase().PublishAsync(BahamutPubSubUtil.AddChannelWithPrefix(appChannel), JsonConvert.SerializeObject(message, Formatting.None));
             });
         }
-        
+
         public void UnSubscribe(string channel)
         {
             Task.Run(async () =>
             {
-                await pubsubRedis.GetDatabase().PublishAsync(AddChannelWithPrefix(channel), UnSubscribeMessage);
+                await pubsubRedis.GetDatabase().PublishAsync(BahamutPubSubUtil.AddChannelWithPrefix(channel), UnSubscribeMessage);
             });
         }
 
